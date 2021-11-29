de9e2527951210aba726aeb87a772900c99d76ab
Jairo Tunisse <jairo_tunisse@outlook.com>
2021-11-29 18:40:11 -0300

    improviments: project idea finish :boom:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import '../styles/header.scss'
+import { TaskList } from './TaskList';
+
+import { FiSun, FiMoon } from 'react-icons/fi'
+
 
 export function Header() {
+
+  const [theme, setTheme] = useState('light');
+
+  function handleSetTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }  
+
   return (
-    <header className="header">
-      <div>
-        <img src="/logo.svg" alt="to.do"/>
-      </div>
-    </header>
+    <div className={`body-content ${theme === 'light' ? 'theme-body-light' : 'theme-body-dark'}`}>
+      <header className={`header ${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
+        <div className={theme === 'light' ? 'light-theme' : 'dark-theme'}>
+          <img src="/logo.svg" alt="to.do" />
+          <button onClick={handleSetTheme} className="select-theme">{theme === 'light' ? <FiMoon className='light-icon'/> : <FiSun className='dark-icon'/>}</button>
+        </div>
+      </header>
+
+      <TaskList th={theme} />
+    </div>
   )
 }
